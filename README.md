e9047917b2566d0995b2373bf8e7fbfd1b0855a6
Mannath93478 <63714965+Mannath93478@users.noreply.github.com>
2024-07-16 09:59:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,55 @@
+# To-Do-List-App
+
+## Description 
+
+📝MyToDoList: A responsive to-do list website 🌐📱 built using HTML, CSS, and JS. Save your tasks and data will present until cleared.
+
+## Authors
+
+- [Mannath Shaik](https://www.github.com/mannath93478) 
+
+
+## Demo
+
+Live Demo:
+
+
+
+![alt text](images/demo.png)
+    
+## Getting Started
+
+To explore or modify the code, follow these steps:
+
+1.Clone the repository
+
+2.Open the project in your preferred code editor.
+
+3.Make changes to the HTML, CSS, or JavaScript files as desired.
+
+4.Test the changes locally.
+
+5.Deploy the updated website to your preferred hosting platform.
+
+
+## Features
+
+1️⃣ Create and manage tasks easily.
+
+2️⃣ Mark tasks as completed for progress tracking.
+
+3️⃣ Responsive design for seamless usage across devices.
+
+4️⃣ Save tasks and data persistently until cleared.
+
+
+## 🔗 Links
+
+For any questions or inquiries, please feel free to reach out. 
+
+Mannath Shaik :
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/mannath-shaik)
+
+
+Thank you for visiting the page!
---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+ .----------------.  .----------------.  .-----------------. .-----------------. .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| | ____    ____ | || |      __      | || | ____  _____  | || | ____  _____  | || |      __      | || |  _________   | || |  ____  ____  | |
+| ||_   \  /   _|| || |     /  \     | || ||_   \|_   _| | || ||_   \|_   _| | || |     /  \     | || | |  _   _  |  | || | |_   ||   _| | |
+| |  |   \/   |  | || |    / /\ \    | || |  |   \ | |   | || |  |   \ | |   | || |    / /\ \    | || | |_/ | | \_|  | || |   | |__| |   | |
+| |  | |\  /| |  | || |   / ____ \   | || |  | |\ \| |   | || |  | |\ \| |   | || |   / ____ \   | || |     | |      | || |   |  __  |   | |
+| | _| |_\/_| |_ | || | _/ /    \ \_ | || | _| |_\   |_  | || | _| |_\   |_  | || | _/ /    \ \_ | || |    _| |_     | || |  _| |  | |_  | |
+| ||_____||_____|| || ||____|  |____|| || ||_____|\____| | || ||_____|\____| | || ||____|  |____|| || |   |_____|    | || | |____||____| | |
+| |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+
+*/
+
+const inputBox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listcontainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+function showTask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
